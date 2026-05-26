129b7ecd80afcd0abb2d7c883839625bcbb0538c
Lm060112 <1709462250@qq.com>
2026-05-26 17:13:51 +0800

    配置云开发环境

--- miniprogram/envList.js
+++ miniprogram/envList.js
@@ -1,6 +1,13 @@
-const envList = [];
+const envList = [
+  {
+    envId: "cloud1-d3gh3r7nm520f7c2b",
+    alias: "dev",
+  },
+];
+
 const isMac = false;
+
 module.exports = {
   envList,
-  isMac
+  isMac,
 };
